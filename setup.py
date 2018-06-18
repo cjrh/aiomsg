cd83ca1d1d99558d199c3cae6eb5ef71ada90a5d
Caleb Hattingh <caleb.hattingh@gmail.com>
2018-06-18 22:01:09 +1000

    Simplify setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,12 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-# Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 from os import path
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
-# Python 3 only projects can skip this import
-from io import open
 
 here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
 
 setup(
     name='aiosmartsock',
@@ -42,6 +31,6 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
     extras_require={
         'dev': ['check-manifest'],
-        'test': ['pytest', 'pytest-cov', 'portpicker'],
+        'test': ['pytest', 'pytest-cov', 'portpicker', 'pytest-benchmark'],
     },
 )
