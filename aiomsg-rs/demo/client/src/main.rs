8d684da862d4d044a79aa79cb04af2c633891a77
Caleb Hattingh <caleb.hattingh@gmail.com>
2020-10-24 22:31:18 +1000

    Simplify the main file

--- aiomsg-rs/demo/client/src/main.rs
+++ aiomsg-rs/demo/client/src/main.rs
@@ -4,12 +4,10 @@ use std::time::Duration;
 
 
 async fn test1() -> Result<()> {
-    println!("Hello, world!");
     let sock = Socket::new();
     println!("sock: {:?}", &sock);
-    let cfg = PeerConfig::new("127.0.0.1", 61111);
-    sock.connect(&cfg).await?;
-    async_std::task::sleep(Duration::from_millis(1000)).await;
+    sock.connect(&PeerConfig::new("127.0.0.1", 61111)).await?;
+    // async_std::task::sleep(Duration::from_millis(1000)).await;
     for _i in 0..100 {
         sock.send(b"test").await?;
         async_std::task::sleep(Duration::from_millis(1000)).await;
